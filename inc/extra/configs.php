b53d7982192be377484e7654fb8f6d17bd31cde6
Robert McCormack <robertmccormack147@gmail.com>
2023-05-17 18:57:13 +0100

    on single...

--- inc/extra/configs.php
+++ inc/extra/configs.php
@@ -8,8 +8,8 @@
 *
 */
 
-$configs['maintenance_mode'] = 'all';
-$configs['redirect_all_traffic_to_page'] = 2;
+// $configs['maintenance_mode'] = 'all';
+// $configs['redirect_all_traffic_to_page'] = 2;
 // $configs['maintenance_template'] = 'coming_soon.twig'; // only applies when redirect_all_traffic_to_page doesnt exist or is set to false. defaults to maintenance.twig downstream
 
 // Global
